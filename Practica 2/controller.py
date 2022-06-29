39932bd56cd2eaf023ad8b163026d1158e91be14
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-06-29 13:20:35 -0300

    Update controller.py
    #Creacion de la base de datos
    #Creacion de las funciones para crear tabla (create_table), insertar una fila(insert_row), leer las filas (read_rows) o insertar varias filas a la vez (insert_rows).

--- Practica 2/controller.py
+++ Practica 2/controller.py
@@ -1,2 +1,58 @@
 import sqlite3 as sql
 
+def create_db():
+    conn = sql.connect("streamers.db")
+    conn.commit()
+    conn.close()
+
+def create_table():
+    conn = sql.connect("streamers.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE streamers (
+            name text,
+            followers integer,
+            subs integer
+        )"""
+    )
+    conn.commit()
+    conn.close()
+
+def insert_row(nombre, followers, subs):
+    conn = sql.connect("streamers.db")
+    cursor = conn.cursor()
+    instruccion = f"INSERT INTO streamers VALUES ('{nombre}', {followers}, {subs})"
+    cursor.execute(instruccion)
+    conn.commit()
+    conn.close()
+
+def read_rows():
+    conn = sql.connect("streamers.db")
+    cursor = conn.cursor()
+    instruccion = f"SELECT * FROM streamers"
+    cursor.execute(instruccion)
+    datos = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    print(datos)
+
+def insert_rows(streamer_list):
+    conn = sql.connect("streamers.db")
+    cursor = conn.cursor()
+    instruccion = f"INSERT INTO streamers VALUES (?, ?, ?)"
+    cursor.executemany(instruccion, streamer_list)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    # create_db()
+    # create_table()
+    # insert_row("Ibai", 7000000, 25000)
+    # insert_row("Alexelcapo", 800000, 10000)
+    # read_rows()
+    # streamers = [
+    #     ("Knekro", 500000, 9500),
+    #     ("Cristinini", 3000000, 5500),
+    #     ("AuronPlay", 8000000, 20000)
+    # ]
+    # insert_rows(streamers)
